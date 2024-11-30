25db2c30768afa1f70841b7d49fec78b6428d09e
Lauriichan <lauriichan@playuniverse.org>
2024-11-30 16:01:25 +0100

    Remove unnecessary imports

--- src/main/java/me/lauriichan/build/builder/Builder.java
+++ src/main/java/me/lauriichan/build/builder/Builder.java
@@ -12,8 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
